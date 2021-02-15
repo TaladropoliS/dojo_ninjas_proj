e0fe9510147c7213cc94c39a8d08bb775e40e874
TaladropoliS <fernando.taladriz@gmail.com>
2021-02-15 19:40:15 -0300

    Se agregó el Dojo: Valdivia Futbol Club

--- dojo_ninjas_app/models.py
+++ dojo_ninjas_app/models.py
@@ -5,6 +5,7 @@ class Dojos(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.CharField(max_length=255, default="Dojo antiguo")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
